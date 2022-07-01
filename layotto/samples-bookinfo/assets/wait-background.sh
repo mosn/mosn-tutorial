513008d9bf84c4a70388ad45cc3ac1dd7a02dd4f
seeflood <zhou.qunli@foxmail.com>
2022-07-01 22:16:52 +0800

    bookinfo
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

---
+++ layotto/samples-bookinfo/assets/wait-background.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# waits for background init to finish
+
+rm $0
+
+clear
+
+echo -n "Initialising Kubernetes..."
+while [ ! -f /ks/.k8sfinished ]; do
+    echo -n '.'
+    sleep 1;
+done;
+echo " done"
+
+echo -n "Initialising Scenario..."
+while [ ! -f /ks/.initfinished ]; do
+    echo -n '.'
+    sleep 1;
+done;
+echo " done"
+
+echo
---
+++ layotto/samples-bookinfo/init/foreground.sh
@@ -0,0 +1,7 @@
+FILE=/ks/wait-background.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
+export ISTIO_VERSION=1.13.3
+curl -L https://istio.io/downloadIstio | TARGET_ARCH=x86_64 sh -
+echo "export PATH=/root/istio-${ISTIO_VERSION}/bin:\$PATH" >> .bashrc
+export PATH=/root/istio-${ISTIO_VERSION}/bin:$PATH
+mv /tmp/demo.yaml /root/istio-${ISTIO_VERSION}/manifests/profiles/
+istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests
