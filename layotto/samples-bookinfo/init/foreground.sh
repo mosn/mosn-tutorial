06e27abb46dd65e988b6edf946ad6ea29febcae4
seeflood <zhou.qunli@foxmail.com>
2022-07-01 22:25:18 +0800

    downgrade istio version to 1.10.6
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

--- layotto/samples-bookinfo/init/foreground.sh
+++ layotto/samples-bookinfo/init/foreground.sh
@@ -1,5 +1,5 @@
 FILE=/ks/wait-background.sh; while ! test -f ${FILE}; do clear; sleep 0.1; done; bash ${FILE}
-export ISTIO_VERSION=1.13.3
+export ISTIO_VERSION=1.10.6
 curl -L https://istio.io/downloadIstio | TARGET_ARCH=x86_64 sh -
 echo "export PATH=/root/istio-${ISTIO_VERSION}/bin:\$PATH" >> .bashrc
 export PATH=/root/istio-${ISTIO_VERSION}/bin:$PATH
