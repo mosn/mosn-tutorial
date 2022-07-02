e5cf4456971f8ef7ece85c36961e69e7d456631f
seeflood <zhou.qunli@foxmail.com>
2022-07-02 11:39:32 +0800

    fix mosn image tag
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

--- layotto/mosn-with-istio/init/foreground.sh
+++ layotto/mosn-with-istio/init/foreground.sh
@@ -3,4 +3,4 @@ export ISTIO_VERSION=1.10.6
 curl -L https://istio.io/downloadIstio | TARGET_ARCH=x86_64 sh -
 echo "export PATH=/root/istio-${ISTIO_VERSION}/bin:\$PATH" >> .bashrc
 export PATH=/root/istio-${ISTIO_VERSION}/bin:$PATH
-istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests --set .values.global.proxy.image="mosnio/proxyv2:1.0.0-1.10.6" --set meshConfig.defaultConfig.binaryPath="/usr/local/bin/mosn"
+istioctl install --set profile=demo -y --manifests=/root/istio-${ISTIO_VERSION}/manifests --set .values.global.proxy.image="mosnio/proxyv2:v1.0.0-1.10.6" --set meshConfig.defaultConfig.binaryPath="/usr/local/bin/mosn"
